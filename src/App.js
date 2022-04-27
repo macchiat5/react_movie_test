d692ff2bfb542e23a1d1241b10b18e592776aae4
chiato mac <chiatomac@chiatoui-Macmini.local>
2022-04-27 21:33:29 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-
-  const onClick = () => setValue(counter + 1);
-  const onChange = (event) => {
-    setKeyword(event.target.value);
-  }
-  
-  useEffect(() => {
-    console.log("i run all the time");
-  }, []);
-  
-  useEffect(() => {
-    console.log(" Counter ..." + counter);
-  }, [counter]);
-
-  useEffect(() => {
-    console.log(" Keyword ..." + keyword);
-  }, [keyword]);
-
-useEffect(() => {
-    console.log(" complex ...");
-  }, [counter, keyword]);
-
+ 
   return (
     <div>
-      <input
-        value={keyword} 
-        onChange={onChange} 
-        type="text" 
-        placeholder="Search here ..." 
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click Me</button>
+      
     </div>  
   );
 }
