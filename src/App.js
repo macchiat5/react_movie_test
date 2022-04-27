ead19ab297f733c762c5a7fc6adf04a11722f88c
chiato mac <chiatomac@chiatoui-Macmini.local>
2022-04-27 22:11:14 +0900

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+	useEffect(() => {
+		console.log(" Created! ");
+		return () => {
+			console.log(" Destroyed! ");
+		}
+	}, []);
+	
+	return (
+		<h1>Hello</h1>
+	);
+}
+
 function App() {
- 
-  return (
-    <div>
-      
-    </div>  
-  );
+	const [showing, setShowing] = useState(false);
+	const onClick = () => {
+    	setShowing((prev) => !prev);
+	}		
+
+	return (
+		<div>
+			{ showing ? <Hello /> : null }
+      		<button onClick={onClick}>{ showing ? "hide" : "show" }</button>
+    	</div>
+  	);
 }
 
 export default App;
